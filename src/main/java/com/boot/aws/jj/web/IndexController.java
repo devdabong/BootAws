29ea37083ddd63eceb765d61dd42dbb126fcb83c
jj0616 <jj0616@ideabeans.co.kr>
2022-05-09 16:20:46 +0900

    @LoginUser 어노테이션 사용 및 db 세션 설정 추가

--- src/main/java/com/boot/aws/jj/web/IndexController.java
+++ src/main/java/com/boot/aws/jj/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boot.aws.jj.web;
 
+import com.boot.aws.jj.config.auth.LoginUser;
 import com.boot.aws.jj.config.auth.dto.SessionUser;
 import com.boot.aws.jj.service.posts.PostsService;
 import com.boot.aws.jj.web.dto.PostsResponseDto;
@@ -28,14 +29,17 @@ public class IndexController {
     /**
      * src/main/resources/templates/index.mustache로 전환되어 View Resolver가 처리하게 됨.
      * ViewResolver : URL 요청 결과를 전달할 타입과 값을 지정하는 관리자 격.
-     * @return
+     *
+     * @LoginUser SessionUser user : 기존에 (SessionUser) httpSession.getAttribute("user")로 가져오던 로직 개선. 어느 컨트롤러든지 @LoginUser만 사용하면 세션 정보 GET
+     *
+     * @return String
      */
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
 
         //CustomOAuth2UserService에서 로그인 성공 시 세션에 SessionUser를 저장하도록 이전에 구성했었다.
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");    // @LoginUser 어노테이션으로 변경하므로 주석처리.
 
         if (user != null) {
             System.out.println(">>>>>>> " + user.getName());
